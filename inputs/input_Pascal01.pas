6c4804087f1d28e90716ca802b8c2e81404f4dc0
ryan-leal <ryanxd178@gmail.com>
2023-12-17 20:01:08 -0300

    Regex - tokens

--- inputs/input_Pascal01.pas
+++ inputs/input_Pascal01.pas
@@ -4,4 +4,12 @@ var
 	valor2: real;
 begin
 	valor1 := 10;
+	valor1 >= valor2;
+	valor2 < valor3;
+	valor1 = valor2;
+	valor3 <> valor1;
+	valor2 or valor1;
+	valor3 and valor2;
+	valor1 + valor2;
+	valor3 / valor1;
 end. 
--- src/grammar.py
+++ src/grammar.py
@@ -1,18 +1,55 @@
-# import re -> Will be used to grammar
+import re
 import sys # Used to argv
 
 def match(input):
-    palavrasReservadas = "program", "var", "integer", "then", "begin", "end", "real"
-    for palavras in palavrasReservadas:
-        if palavras in input:
-            print(input + ' eh ' + palavras)
+    pascalGrammar = {
+        "program" : "palavra reservada",
+        "var" : "palavra reservada",
+        "integer" : "palavra reservada",
+        "real" : "palavra reservada",
+        "boolean" : "palavra reservada",
+        "procedure" : "palavra reservada",
+        "begin" : "palavra reservada",
+        "end" : "palavra reservada",
+        "if" : "palavra reservada",
+        "then" : "palavra reservada",
+        "else" : "palavra reservada",
+        "while" : "palavra reservada",
+        "do" : "palavra reservada",
+        "not" : "palavra reservada",
+        ";" : "delimitador"
+    }
+    if pascalGrammar[input]:
+        print(input + ' eh ' + pascalGrammar[input])
 
 def getProgramFile(fileInput):
+    patternsPascal = [
+        ('PALAVRA_RESERVADA', r'\b(program|var|integer|real|boolean|procedure|begin|end|if|then|else|while|do|not)\b'),
+        ('ATRIBUICAO', r':='),
+        ('RELACIONAIS', r'(<=|>=|<>|>|<|=)'),
+        ('OPERADORES_ADITIVOS', r'(\+|-|\bor\b)'),
+        ('OPERADORES_MULTIPLICATIVOS', r'(\*|/|\band\b)'),
+        ('DELIMITADOR', r'[;.:\(\),]')
+    ]
     opennedFile = open(fileInput, 'r')
-    data = opennedFile.readlines()
+    data = opennedFile.read()
     print(data)
-    for linhas in range(len(data)):
-        match(data[linhas])
+    line = 1
+    index = 1
+
+    combined_pattern = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in patternsPascal)
+    regex = re.compile(combined_pattern)
+
+    for match in regex.finditer(data):
+        start, end = match.span()
+        index = end - data.rfind('\n', 0, start)
+        # Atualize a linha atual
+        line += data.count('\n', start, end)
+        print(data.count('\n', start, end))
+        print(str(line) + ' ' + match.group() + ' ' + match.lastgroup)
+
+        
+
     
       
 
