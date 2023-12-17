68c8b21f4e200882d6b1e3af4b274fea3ef89474
ryan-leal <ryanxd178@gmail.com>
2023-12-17 15:40:59 -0300

    Simple Match

--- src/grammar.py
+++ src/grammar.py
@@ -1,10 +1,19 @@
 # import re -> Will be used to grammar
 import sys # Used to argv
 
+def match(input):
+    palavrasReservadas = "program", "var", "integer", "then", "begin"
+    for palavras in palavrasReservadas:
+        if palavras in input:
+            print(input + ' eh ' + palavras)
+
 def getProgramFile(fileInput):
     opennedFile = open(fileInput, 'r')
-    data = opennedFile.read()
+    data = opennedFile.readlines()
     print(data)
+    for linhas in range(len(data)):
+        match(data[linhas])
+    
       
 
 def main():
