d21708d462e314430ad26c5d939097fdaabcd335
ryan-leal <ryanxd178@gmail.com>
2023-12-20 11:30:34 -0300

    Grammar v1.0

--- src/grammar.py
+++ src/grammar.py
@@ -1,67 +1,101 @@
-import re
+import re # Used in regex functions
 import sys # Used to argv
-from tabulate import tabulate
+from tabulate import tabulate # Create tables
 
-def match(input):
-    pascalGrammar = {
-        "program" : "palavra reservada",
-        "var" : "palavra reservada",
-        "integer" : "palavra reservada",
-        "real" : "palavra reservada",
-        "boolean" : "palavra reservada",
-        "procedure" : "palavra reservada",
-        "begin" : "palavra reservada",
-        "end" : "palavra reservada",
-        "if" : "palavra reservada",
-        "then" : "palavra reservada",
-        "else" : "palavra reservada",
-        "while" : "palavra reservada",
-        "do" : "palavra reservada",
-        "not" : "palavra reservada",
-        ";" : "delimitador"
-    }
-    if pascalGrammar[input]:
-        print(input + ' eh ' + pascalGrammar[input])
+# Function to print error messages
+def error_msg(error_Type, error_Token, errorLine):
+    if error_Type == 'ERROR_INVALID_TOKEN':
+        print(f'ERROR: {error_Type} {error_Token} IN LINE {errorLine}')
+    else:
+        print(f'ERROR: {error_Type} BUT NOT CLOSED IN LINE {errorLine}')
 
-def getProgramFile(fileInput):
+
+def build_regex():
+    # All pascal regex in groups
     patternsPascal = [
-        ('COMENTARIO', r'{[\S\s]*?}'),
-        ('PALAVRA_RESERVADA', r'\b(program|var|integer|real|boolean|procedure|begin|end|if|then|else|while|do|not)\b'),
+        ('COMMENT', r'{[\S\s]*?}'),
+        ('OPENNED_COMMENT', r'{[\S\s]*[^}]'),
+        ('RESERVED_WORD', r'\b(program|var|integer|real|boolean|procedure|begin|end|if|then|else|while|do|not)\b'),
         ('REAL', r'\b\d+\.\d*'),
-        ('INTEIRO', r'\b\d+\b'),
-        ('ATRIBUICAO', r':='),
-        ('RELACIONAIS', r'(<=|>=|<>|>|<|=)'),
-        ('OPERADORES_ADITIVOS', r'(\+|-|\bor\b)'),
-        ('OPERADORES_MULTIPLICATIVOS', r'(\*|/|\band\b)'),
-        ('IDENTIFICADOR', r'\b[a-z][a-zA-Z0-9_]*\b'),
-        ('DELIMITADOR', r'[;.:\(\),]')
+        ('INTEGER', r'\b\d+\b'),
+        ('ASSIGNMENT', r':='),
+        ('RELATIONAL_OPERATOR', r'(<=|>=|<>|>|<|=)'),
+        ('ADDITIVE_OPERATOR', r'(\+|-|\bor\b)'),
+        ('MULTIPLICATION_OPERATOR', r'(\*|/|\band\b)'),
+        ('IDENTIFIER', r'\b[a-z][a-zA-Z0-9_]*\b'),
+        ('DELIMITER', r'[;.:\(\),]'),
+        ('ERROR_INVALID_TOKEN',r'[^A-Za-z0-9=<>:;_\+\-\*\/{}\t\s.]')
     ]
-    opennedFile = open(fileInput, 'r')
-    data = opennedFile.read()
-    line = 1
-    index = 1
-    headers=['TOKEN ', 'CLASSIFICAÇÃO', 'LINHA']
-    tokens = []
+
+    # Combine all regex groups using OR operator, compile and return the regex
     combined_pattern = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in patternsPascal)
     regex = re.compile(combined_pattern)
-    
-    print()
-    for match in regex.finditer(data):
-        start, end = match.span()
-        index = end - data.rfind('\n', 0, start)
-        line += data.count('\n', 0, end)
-        tokens.append((match.group(), match.lastgroup, str(line)))
-        line = 1
+    return regex
 
+def match(dataInput):
+    # Regex build to be used in matching loop
+    regex = build_regex()
+
+    # Result tokens and classifications
+    tokens = []
+
+    # tokens that don't will be in result
+    nonListable_tokens = ('ERROR_INVALID_TOKEN', 'COMMENT', 'OPENNED_COMMENT')
+
+    # Line and index used in lexer
+    line = 1
+    #index = 1 -> Can be useful in future...
+
+    # For every match object in find iteration inside file data
+    for match in regex.finditer(dataInput):
+        # Get the start and end of match
+        start, end = match.span()
+
+        # Calculate line and index using newline char
+        line += dataInput.count('\n', 0, start)
+        # index = end - dataInput.rfind('\n', 0, start) -> Maybe in Future
+
+        # If the match is a comment or error
+        if match.lastgroup in nonListable_tokens:
+            # if is an error... correct comment don't enter this statement
+            if match.lastgroup != 'COMMENT':
+                error_msg(match.lastgroup, match.group(), line)
+            line = 1
+            continue
+        
+        # Append in result list
+        tokens.append((match.group(), match.lastgroup, str(line)))
+        # Reset line for next loop
+        line = 1
+    # Return tokens, classification and line
+    return tokens
+
+# Using tabulate, creates a table with result tokens classification
+def printTable(tokens):
+    headers=['TOKEN ', 'CLASSIFICATION', 'LINE']
     table = tabulate(tokens ,headers=headers, tablefmt='grid')
     print(table)
-        
+
+# Open and close input file
+def getProgramFile(fileInput):
+    with open(fileInput, 'r') as opennedFile:
+        data = opennedFile.read()
+    return data
+
+def lexer(fileInput):
+    dataInput = getProgramFile(fileInput) # open input file
+    tokens = match(dataInput) # Get tokens from main lexer function
+    printTable(tokens) # Print token table
+
+
 def main():
     args = sys.argv
-    # Printing how arguments works
-    #for arguments in args:
-    #    print(arguments)
-    getProgramFile(args[1])
+    lexer(args[1]) # call the lexical analyzer with path to pascal file
+
+#    if re.search(r'[\s\S]+(\.pas)', args[1]) :
+#       Only pascal files...        
+#    else:
+#        print('Only .pas files allowed')
 
 if __name__ == "__main__":
     main()
