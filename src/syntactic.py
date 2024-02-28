4c13467f340d5125b96b63edcdea634eb68cd009
ryan-leal <ryanxd178@gmail.com>
2024-02-28 08:39:06 -0300

    Updating Main

--- src/syntactic.py
+++ src/syntactic.py
@@ -15,7 +15,9 @@ class tokenAnalyzer:
             return None  # Retorna None se não houver mais tokens
 
 def listIdentifier(isArgs = 0):
-    global analyzer, token 
+    global analyzer, token
+    if isArgs == 1 and token[0] == ')':
+        return 
     if token[1] == 'IDENTIFIER':
         print('Token before next in listIdentifier: ' + token[0])
         token = analyzer.next()
@@ -88,6 +90,9 @@ def subProgramDeclaration():
                             compostCommand()
                             return
                         else:
+                            if token[0] == 'end':
+                                print('Dont found ; but found end...')
+                                return
                             print('ERROR: EXPECTED \';\' BUT ' + token[0] + ' FOUND')
                             sys.exit()
                     else:
@@ -105,19 +110,106 @@ def subProgramDeclaration():
     else:
         print('procedure dont found but ' + token[0])
 
+def expressionAnalyzer():
+    global token, analyzer
+    print('Analysis: ' + token[0])
+    token = analyzer.next()
+
+def commandList():
+    global token, analyzer
+    print('Em command List com token: ' + token[0])
+    if token[1] == 'IDENTIFIER':
+        print('identifier Found, token: ' + token[0])
+        token = analyzer.next()
+        if token[0] == ':=':
+            print('I found :=, so its a attribuition')
+            token = analyzer.next()
+            while not(token[0] == 'end' or token[0] == ';' or token[0] == 'else'):
+                expressionAnalyzer()
+            if token[0]== ';':
+                print('expression analyzes')
+                token = analyzer.next()
+                return
+            elif token[0] == 'end':
+                print('expression analyzes and end found')
+                return
+        elif token[0] == '(':
+            print('its a procedure call using ()')
+            token = analyzer.next()
+            while token[0] != ')':
+                listIdentifier(1)
+            if token[0] == ')':
+                token = analyzer.next()
+                if token[0] == ';':
+                    print('Procedure call found')
+                    token = analyzer.next()
+                    return
+                else:
+                    if token[0] == 'end':
+                        print('Procedure call found')
+                        return
+                    print('ERROR: EXPECTED \';\' BUT ' + token[0] + ' FOUND')
+                    sys.exit()
+        elif token[0] == ';':
+            print('its a procedure call')
+            token = analyzer.next()
+            return
+        else:
+            print('ERROR: EXPECTED \':=\' OR \';\' OR \'();\'')
+    else:
+        if token[1] == 'RESERVED_WORD':
+            print('Reserved Word Found, token: ' + token[0])
+            if token[0] == 'begin':
+                print('begin found inside begin')
+                compostCommand()
+            elif token[0] == 'if':
+                print('i found if command')
+                token = analyzer.next()
+                while token[0] != 'then':
+                    expressionAnalyzer()
+                if token[0] == 'then':
+                    print('i found then')
+                    token = analyzer.next()
+                    while token[0] != 'else':
+                        commandList()
+                    if token[0] == 'else':
+                        print('received else')
+                        token = analyzer.next()
+                        commandList()
+                    else:
+                        print('No else statement found')
+            elif token[0] == 'while':
+                print('Found while command')
+                token = analyzer.next()
+                while token[0] != 'do':
+                    expressionAnalyzer()
+                if token[0] == 'do':
+                    print('Do found')
+                    token = analyzer.next()
+                    commandList()
+        else:
+            print('Options dont matched, error with token: ' + token[0])
+            sys.exit()
 
 def compostCommand():
+    global token, analyzer
     if token[0] == 'begin':
-        print('I found begin')
-    elif token[0] == 'end':
-        print('I found end')
-    else:
-        print('Comandos especiais')
-        optionalCommand()
-        sys.exit()
+        print('found begin')
+        token = analyzer.next()
+        while token[0] != 'end':
+            commandList()
+        print('Get out of while with token: ' + token[0])
+        if token[0] == 'end':
+            print('Found end after found begin, everything okay')
+            token = analyzer.next()
+        else:
+            print('ERROR: EXPECTED \'end\' BUT ' + token[0] + ' IN LINE ' + token[2])
+            sys.exit()
 
-def optionalCommand():
-    print('Eu sou um comando opcional')
+    else:
+        print('ERROR: EXPECTED \'begin\' BUT ' + token[0] + ' IN LINE ' + token[2])
+        sys.exit()
+    print('No comando Composto')
 
 def synAnalysis(tokenList):
     # Instance of tokenAnalyzer to return next token using token list from lexer
